7c5ba24f66d9696f6e70aaadecc29c2ea68fe91f
qiantao <532661800@qq.com>
2016-01-07 19:11:18 +0800

    Initial Commit

---
+++ LearningUIkitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningUIkitApp
+//
+//  Created by qian tao on 16/1/7.
+//  Copyright © 2016年 qian tao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
